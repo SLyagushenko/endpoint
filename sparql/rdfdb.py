dbc9380fc1d7adf2d256cbf3cb26b89f53cd2a0d
SLyagushenko <s.lyagushenko@gmail.com>
2018-02-27 19:33:24 +0300

    Initial commit

---
+++ sparql/rdfdb.py
@@ -0,0 +1,46 @@
+import io
+import json
+from rdflib import plugin, ConjunctiveGraph, Literal, URIRef
+from rdflib.store import Store
+from rdflib.plugins.sparql.results import csvresults, jsonresults, tsvresults, xmlresults
+from rdflib_sqlalchemy import registerplugins
+
+class RDFdb:
+
+    graph = ''
+
+    def __init__(self):
+        registerplugins()
+        identifier = URIRef("rdfdb")
+        db_uri = Literal('cockroachdb://lab04:26257/rdfdb?sslmode=verify-full&sslcert=/etc/cockroachdb/certs/client.root.crt&sslkey=/etc/cockroachdb/certs/client.root.key&sslrootcert=/etc/cockroachdb/certs/ca.crt')
+        store = plugin.get("SQLAlchemy", Store)(identifier=identifier, configuration=db_uri)
+        self.graph = ConjunctiveGraph(store)
+        self.graph.open(db_uri, create=True)
+
+    def get_result_json(self, query): 
+        result = io.StringIO()        
+        temp_result = self.graph.query(query)        
+        serial = jsonresults.JSONResultSerializer(temp_result)
+        serial.serialize(result)
+        _json = result.getvalue()
+        result.close()
+        _json = json.loads(_json)        
+        return _json            
+
+    def get_result_xml(self, query):
+        result = io.BytesIO()        
+        temp_result = self.graph.query(query)
+        serializer = xmlresults.XMLResultSerializer(temp_result)      
+        serializer.serialize(result)
+        _xml = result.getvalue()
+        result.close()
+        return _xml
+    
+    def get_result_csv(self, query): 
+        result = io.BytesIO()        
+        temp_result = self.graph.query(query)
+        serializer = csvresults.CSVResultSerializer(temp_result)      
+        serializer.serialize(result)
+        _csv = result.getvalue()
+        result.close()  
+        return _csv
---
+++ sparql/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+from django.http import HttpResponse, JsonResponse, QueryDict, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.template import Context, loader, RequestContext
+from sparql import rdfdb
+
+request_types = ['application/sparql-results+json', 'application/sparql-results+xml', 'text/csv', 'text/tab-separated-values']
+
+@csrf_exempt
+def index(request):
+    http_accept = request.META.get('HTTP_ACCEPT')
+    if any(request_type in http_accept for request_type in request_types):
+        if request.method == 'GET':    
+            return HttpResponse('GET Not Implemented')
+
+        elif request.method == 'POST':        
+            q = QueryDict(request.body)
+            query = list(q.items())[0][1]        
+            db = rdfdb.RDFdb()
+            if request_types[0] in http_accept:
+                _json = db.get_result_json(query)
+                response =  JsonResponse(_json, content_type = request_types[0])
+                response['Content-Length'] = len(json.dumps(_json))
+            elif request_types[1] in http_accept:
+                _xml = db.get_result_xml(query)
+                response = HttpResponse(_xml, content_type = request_types[1])
+                response['Content-Length'] = len(_xml)
+            elif request_types[2] in http_accept:
+                _csv = db.get_result_csv(query)
+                response = HttpResponse(_csv, content_type = request_types[2])
+                response['Content-Length'] = len(_csv)
+            elif request_types[3] in http_accept:
+                response = HttpResponseForbidden('TSV Not Implemented')
+            else: response = HttpResponseForbidden('403 Forbidden')
+            return response
+    else:
+        return HttpResponseForbidden('403 Forbidden')
+
+    return HttpResponse('Not Implemented')
